7f1555a0cae03180b2749cf48ca7b8546b7e1f01
John Reese <john@noswap.com>
2013-02-26 18:27:26 -0800

    Get base gcm service working, now with logcat

--- client/Keyring/src/com/noswap/keyring/GCMIntentService.java
+++ client/Keyring/src/com/noswap/keyring/GCMIntentService.java
@@ -1,32 +1,39 @@
 package com.noswap.keyring;
 
-import com.google.android.gcm.GCMBaseIntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gcm.GCMBaseIntentService;
 
 public class GCMIntentService extends GCMBaseIntentService
 {
+	public static final String TAG = "Keyring";
+
 	@Override
 	public void onRegistered(Context context, String regId)
 	{
+		Log.v(TAG, "onRegistered: " + regId);
 		// TODO: send regId to server
 	}
 
 	@Override
 	public void onUnregistered(Context context, String regId)
 	{
+		Log.v(TAG, "onUnRegistered: " + regId);
 		// TODO: remove regId from server
 	}
 
 	@Override
 	public void onMessage(Context context, Intent intent)
 	{
-		// Do something with the message
+		Log.v(TAG, "onMessage: ");
 	}
 
 	@Override
 	public void onError(Context context, String errorId)
 	{
+		Log.v(TAG, "onError: " + errorId);
 	}
 
 	//@Override
--- client/Keyring/src/com/noswap/keyring/MainActivity.java
+++ client/Keyring/src/com/noswap/keyring/MainActivity.java
@@ -2,14 +2,31 @@ package com.noswap.keyring;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gcm.GCMRegistrar;
 
 public class MainActivity extends Activity
 {
+	public static final String TAG = "Keyring";
+	public static final String SENDER_ID = "192076224105";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		GCMRegistrar.checkDevice(this);
+		GCMRegistrar.checkManifest(this);
+
+		final String regId = GCMRegistrar.getRegistrationId(this);
+		if (regId.equals("")) {
+			Log.v(TAG, "Registering");
+			GCMRegistrar.register(this, SENDER_ID);
+		} else {
+			Log.v(TAG, "Already registered");
+		}
     }
 }
